f409e9aa0f08d0d60959d7e69ae8f96304d78b73
ngf93 <nabiullinaguzel93@gmail.com>
2021-07-16 18:07:43 +0300

    save 1

---
+++ js/quiz.js
@@ -0,0 +1,16 @@
+
+
+// var modalQuiz = document.getElementById('quiz');
+// modalQuiz.addEventListener('show.bs.modal', function (event) {
+//   const quiz = new Swiper('.myQuiz', {
+//     spaceBetween: 30,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       // type: "fraction",
+//     },
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//   });
+// })
