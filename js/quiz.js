7a0980e6bd87550c185bc7df0ccdf22b88705a15
ngf93 <nabiullinaguzel93@gmail.com>
2021-07-19 18:12:14 +0300

    save 2

--- js/quiz.js
+++ js/quiz.js
@@ -1,16 +1,32 @@
+var modalQuiz = document.getElementById('quiz');
+modalQuiz.addEventListener('show.bs.modal', function (event) {
+    let arr_slides = Array.from(document.querySelectorAll('.carousel-item'));
+    let count = arr_slides.length;
+    document.getElementById('currentStep').append('1');
+    document.getElementById('allSteps').append(count);
 
+    /* indicator */
+    arr_slides.forEach(function(item, i, arr) {
+        let indicators = item.querySelector('.indicators');
+        let activeCount = i+1;
+        let notActive = count-activeCount;
+        let j;
+        for(j = 0; j<activeCount; j++){
+            indicators.insertAdjacentHTML('beforeend', '<div class="active"></div>');
+        }
+        for(j = 0; j<notActive; j++){
+            indicators.insertAdjacentHTML('beforeend', '<div></div>');
+        }
+    });
 
-// var modalQuiz = document.getElementById('quiz');
-// modalQuiz.addEventListener('show.bs.modal', function (event) {
-//   const quiz = new Swiper('.myQuiz', {
-//     spaceBetween: 30,
-//     pagination: {
-//       el: ".swiper-pagination",
-//       // type: "fraction",
-//     },
-//     navigation: {
-//       nextEl: ".swiper-button-next",
-//       prevEl: ".swiper-button-prev",
-//     },
-//   });
-// })
+    /* current slide index */
+    modalQuiz.addEventListener('slid.bs.carousel', function () {
+        for(let y = 0; y<count; y++){
+            if(arr_slides[y].classList.contains('active')){
+                let slideNum = y+1;
+                document.getElementById('currentStep').innerHTML = slideNum;
+            }
+        }
+    })
+});
+
