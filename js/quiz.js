31bb85e63a9e7727b5372f887caf6e1a67daf81d
ngf93 <nabiullinaguzel93@gmail.com>
2021-07-21 18:07:32 +0300

    save 3

--- js/quiz.js
+++ js/quiz.js
@@ -2,31 +2,65 @@ var modalQuiz = document.getElementById('quiz');
 modalQuiz.addEventListener('show.bs.modal', function (event) {
     let arr_slides = Array.from(document.querySelectorAll('.carousel-item'));
     let count = arr_slides.length;
-    document.getElementById('currentStep').append('1');
-    document.getElementById('allSteps').append(count);
 
-    /* indicator */
-    arr_slides.forEach(function(item, i, arr) {
-        let indicators = item.querySelector('.indicators');
-        let activeCount = i+1;
-        let notActive = count-activeCount;
-        let j;
-        for(j = 0; j<activeCount; j++){
-            indicators.insertAdjacentHTML('beforeend', '<div class="active"></div>');
-        }
-        for(j = 0; j<notActive; j++){
-            indicators.insertAdjacentHTML('beforeend', '<div></div>');
-        }
-    });
-
-    /* current slide index */
-    modalQuiz.addEventListener('slid.bs.carousel', function () {
+    /* finding current page number */
+    function findCurrent(){
         for(let y = 0; y<count; y++){
             if(arr_slides[y].classList.contains('active')){
                 let slideNum = y+1;
-                document.getElementById('currentStep').innerHTML = slideNum;
+                return slideNum;
             }
         }
+    }
+
+    /* the initial values of the slider pages */
+    document.getElementById('currentStep').innerHTML = findCurrent();
+    document.getElementById('allSteps').innerHTML = count;
+
+    /* indicator */
+    arr_slides.forEach(function(item, i, arr) {
+        let indicator = item.querySelector('.indicators');
+        let activeCount = i+1;
+        let notActive = count-activeCount;
+        let j;
+        let elems = [];
+        
+        for(j = 0; j<activeCount; j++){
+            elems.push('<div class="active"></div>');
+        }
+        for(j = 0; j<notActive; j++){
+            elems.push('<div></div>');
+        }
+
+        indicator.innerHTML = elems.join(' ');
+    });
+
+    /* updating the page numbers of the slider when paging */
+    modalQuiz.addEventListener('slid.bs.carousel', function () {
+        document.getElementById('currentStep').innerHTML = findCurrent();
     })
 });
 
+
+/* проверка input-ов с атрибутом required и активация/блокировка кнопки submit */
+function verifyInput(form){
+    let arr_inputs = Array.from(form.querySelectorAll('input[required]'));
+    console.log(arr_inputs.length);
+  
+    function notNull(element, index, array) {
+      if(element.type == 'checkbox' && element.checked){
+        return element;
+      } else if(element.type == 'text' && element.value.trim() != '') {
+        return element;
+      }
+    }
+    let flag = arr_inputs.every(notNull);
+  
+    if (flag) {
+      console.log('все поля заполнены');
+      form.querySelector('button[type="submit"]').removeAttribute('disabled');
+    } else {
+      console.log('есть не заполненые поля');
+      form.querySelector('button[type="submit"]').setAttribute('disabled', 'disabled');
+    }
+  }
